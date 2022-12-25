cac71c53c06b688c893545965ec31ddc6b9ab2f7
Minh8x8 <105042689+Minh8x8@users.noreply.github.com>
2022-12-25 16:12:39 +0700

    save before demo

--- src/test/StudentManagementControllerTest.java
+++ src/test/StudentManagementControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import view.StudentManagementView;
 
+import java.io.File;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,11 +32,7 @@ class StudentManagementControllerTest {
         view.addStudentToTable(studentExpected2);
         view.model.addStudent(studentExpected2);
     }
-    @Test
-    void addButton() {
-        // Check the table have the Student information or not
 
-    }
     @Test
     void getStudentFromTableTest() {
         view.table.setRowSelectionInterval(0,0);
@@ -64,6 +61,7 @@ class StudentManagementControllerTest {
 
     @Test
     void deleteButton() {
+        // Set row selection interval
         view.table.setRowSelectionInterval(0,0);
         controller.deleteButton();
         // Check the rows is 1 or not
@@ -81,16 +79,29 @@ class StudentManagementControllerTest {
 
     @Test
     void clearSearchButton() {
+        // Check if the table after clear search button has two rows or not
         controller.clearSearchButton();
         int rows = view.table.getRowCount();
         assertEquals(2,rows);
     }
 
     @Test
-    void menupen() {
+    void menuOpen() {
+        // Run openFile method to open the file
+        controller.openFile();
+        File fileExpected = new File("C:/Users/Minh/OneDrive/Documents/fileTest.txt");
+        String actualPath = view.model.getFileName();
+        // Check if the model get the correct path or not
+        File fileActual = new File (actualPath);
+        assertEquals(fileExpected, fileActual);
     }
 
     @Test
     void menuSave() {
+        // Run saveFile function and save the file with the name fileSaveTest.txt
+        controller.saveFile();
+        File expected = new File("C:/Users/Minh/OneDrive/Documents/fileSaveTest.txt");
+        // Check if the file is existing or not
+        assertTrue(expected.exists());
     }
 }
